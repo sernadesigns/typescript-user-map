57438ae08953477c8add7e530c9eb00147e316d7
sernadesigns <sernadesigns@gmail.com>
2021-05-29 00:41:07 -0600

    Adds 'implements' clause to both User and Company classes

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,11 +1,11 @@
 // Instructions to every other class
 // on how they can be an argument to 'addMarker'
-interface Mappable {
+export interface Mappable {
 	location: {
 		lat: number,
 		lng: number
 	};
-	markerContent(): string
+	markerContent(): string;
 }
 
 export class CustomMap {
